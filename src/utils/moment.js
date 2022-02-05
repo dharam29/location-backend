e43a9b9bee28aef7c0736b3c1aa6a8632cfc6aa4
dharam29 <dharmendra.developer1989@gmail.com>
2022-02-05 18:02:31 +0530

    Refactor: Removed

--- src/utils/moment.js
+++
@@ -1,7 +0,0 @@
-import moment from 'moment';
-
-export const archiveTime = (day) => {
-  if(day === null) return null;
-  const archiveTime = moment().add(day, 'days').format('YYYY-MM-DD HH:mm');
-  return archiveTime;
-};
